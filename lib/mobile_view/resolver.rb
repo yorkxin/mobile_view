3ace145fffa356bea6a7fa91c5a3389528e3b1c2
chitsaou <ducksteven@gmail.com>
2012-12-22 13:36:30 +0800

    note the origin of MOBILE_PATTERN

--- lib/mobile_view/resolver.rb
+++ lib/mobile_view/resolver.rb
@@ -1,5 +1,7 @@
 module MobileView
   class Resolver < ::ActionView::FileSystemResolver
+    # pattern is modified from ActionView
+    # https://github.com/rails/rails/blob/v3.2.9/actionpack/lib/action_view/template/resolver.rb#L106
     MOBILE_PATTERN = ":prefix/:action.mobile{.:locale,}{.:formats,}{.:handlers,}"
 
     def initialize(path)
