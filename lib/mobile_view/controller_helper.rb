7bc5a4184b9098a534609394748b3cbd5377f5d3
chitsaou <ducksteven@gmail.com>
2012-12-22 16:31:35 +0800

    refactor interpolation steps of controller helpers
    This makes it possible to have other mounted engines reuse controller helpers.
    
    Inspired from Devise:
    https://github.com/plataformatec/devise/blob/master/lib/devise/controllers/helpers.rb

--- lib/mobile_view/controller_helper.rb
+++ lib/mobile_view/controller_helper.rb
@@ -1,5 +1,17 @@
 module MobileView
   module ControllerHelper
+    extend ActiveSupport::Concern
+
+    included do
+      # make mobile? method a view helper too
+      helper_method :mobile?
+
+      # find mobile view templates first, if mobile device is detected.
+      before_filter do
+        prepend_view_path(MobileView.resolver) if mobile?
+      end
+    end
+
     protected
     def mobile?
       cookies[:mobile].present? || request.headers["X_MOBILE_DEVICE"].present?
--- lib/mobile_view/engine.rb
+++ lib/mobile_view/engine.rb
@@ -8,15 +8,9 @@ module MobileView
     end
 
     initializer "mobile_view.controller_methods" do |app|
-      ActionController::Base.send :include, MobileView::ControllerHelper
-
-      # make mobile? method a view helper too
-      ActionController::Base.send :helper_method, :mobile?
-
-      # find mobile view templates first, if mobile device is detected.
-      ActionController::Base.send :before_filter, Proc.new {
-        prepend_view_path(MobileView.resolver) if mobile?
-      }
+      ActiveSupport.on_load(:action_controller) do
+        include MobileView::ControllerHelper
+      end
     end
   end
 end
